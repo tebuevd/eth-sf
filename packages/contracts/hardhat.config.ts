c2ea05a640fb86162f1a1adfbb922ae8dd657395
dinislam tebuev <3241533+tebuevd@users.noreply.github.com>
2022-11-04 21:28:27 -0700

    add typechain and fix bugs with hardhat install

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,5 +1,8 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
